e7b603464410130537a78d42f5ddc56eb34e2b22
kmoler1 <kmoler1@uwyo.edu>
2024-09-25 21:31:25 +0000

    Practice

---
+++ practice.py
@@ -0,0 +1,10 @@
+names=["Jay", "Jhett", "Kaleb", "Connor", "Jared", "Neila"]
+print(names)
+del names[2]
+print(names)
+names.append("Brady")
+print(names)
+for name in names:
+    print(f"{name} is at my table")
+names.reverse()
+print(names)
